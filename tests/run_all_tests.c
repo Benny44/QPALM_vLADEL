2f5024f2266170e4235e309922dc94a2c8b41865
Ben Hermans <ben.hermans2@student.kuleuven.be>
2021-01-06 16:22:56 +0100

    Port casadi general_convex_sparse_test

--- tests/run_all_tests.c
+++ tests/run_all_tests.c
@@ -12,24 +12,26 @@
 #include "test_ls_qp.h"
 #include "test_medium_qp.h"
 #include "test_casadi_general_unconstrained.h"
+#include "test_casadi_general_convex_sparse.h"
 #include "test_update_after_setup.h"
 
 
 int main(){
     MU_INITIALIZE();
-    // MU_RUN_SUITE(suite_lin_alg);
-    // MU_RUN_SUITE(suite_solver);
-    // MU_RUN_SUITE(suite_basic_qp);
-    // MU_RUN_SUITE(suite_medium_qp);
-    // MU_RUN_SUITE(suite_prim_inf_qp);
-    // MU_RUN_SUITE(suite_dua_inf_qp);
-    // MU_RUN_SUITE(suite_degen_hess);
-    // MU_RUN_SUITE(suite_nonconvex);
-    // MU_RUN_SUITE(suite_update);
-    // MU_RUN_SUITE(suite_validation);
-    // MU_RUN_SUITE(suite_error_handling);
-    // MU_RUN_SUITE(suite_ls_qp);
-    // MU_RUN_SUITE(suite_casadi_general_unconstrained);
+    MU_RUN_SUITE(suite_validation);
+    MU_RUN_SUITE(suite_error_handling);
+    MU_RUN_SUITE(suite_lin_alg);
+    MU_RUN_SUITE(suite_solver);
+    MU_RUN_SUITE(suite_basic_qp);
+    MU_RUN_SUITE(suite_medium_qp);
+    MU_RUN_SUITE(suite_prim_inf_qp);
+    MU_RUN_SUITE(suite_dua_inf_qp);
+    MU_RUN_SUITE(suite_degen_hess);
+    MU_RUN_SUITE(suite_nonconvex);
+    MU_RUN_SUITE(suite_update);
+    MU_RUN_SUITE(suite_ls_qp);
+    MU_RUN_SUITE(suite_casadi_general_unconstrained);
+    MU_RUN_SUITE(suite_casadi_general_convex_sparse);
     MU_RUN_SUITE(suite_update_after_setup);
     MU_REPORT();
     
